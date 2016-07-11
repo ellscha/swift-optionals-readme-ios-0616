b368c480e19e0eea58f9d79f0a7259cd56e9693b
ellscha <escharlin@wesleyan.edu>
2016-07-11 10:49:43 -0400

    added another example.

--- Pets.playground/Contents.swift
+++ Pets.playground/Contents.swift
@@ -16,3 +16,24 @@ if let friendsPet = petName {
     print("My friend's pet is \(friendsPet)")
     print("petName is still an Optional! \(petName)")
 }
+
+
+var momName: String?
+
+momName = "Mommy"
+print(momName)
+
+if momName != nil {
+    print("I call my mom, \(momName)")
+} else {
+    print("her name is Amy")
+}
+if let momName = momName {
+    print("my mom's name is  \(momName)")
+}
+
+if let mommyName = momName {
+    print("my mommy's name is \(mommyName)")
+    print("my mom name is still optional \(momName)")
+
+}
